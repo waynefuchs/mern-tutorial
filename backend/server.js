4d1ebdbaf94749d7d1ba6f09945a5c4d89f8f4fe
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-05-20 20:16:19 -0400

    Add a single endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({ message: "Get goals" })
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
