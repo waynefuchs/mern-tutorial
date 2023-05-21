bacffe29552dfb1a77d12b1db05ed0d83b5113cf
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-05-21 04:07:53 -0400

    Add input parsing middleware

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,12 @@ import goalRoutes from "./routes/goalRoutes.js"
 const port = process.env.PORT || 5000
 const app = express()
 
+// Enable parsing json body and urlencoded inputs
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Routes
 app.use("/api/goals", goalRoutes)
 
+// Start the server
 app.listen(port, () => console.log(`Server started on port ${port}`))
