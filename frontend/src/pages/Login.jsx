22fd5d6ea92e7518d4d01fb0d40caff64e346d38
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-05-23 00:06:57 -0400

    Added front end UI

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,67 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import { FaSignInAlt } from "react-icons/fa"
+
+import "../styles/Form.css"
 
 function Login() {
-  return <div>Login</div>
+  // State
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+
+  // Destructured State (read-only)
+  const { name, email, password, password2 } = formData
+
+  // Helper Functions
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt />
+          Register
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
+
+      <section className="form">
+        <form className="form-group" onSubmit={onSubmit}>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={onChange}
+          />
+
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Enter password"
+            onChange={onChange}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </>
+  )
 }
 
 export default Login
