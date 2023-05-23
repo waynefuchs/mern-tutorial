e0ca3544ff1d14f767af738989ccf44ef2417f42
Wayne Fuchs <wayne.fuchs@icloud.com>
2023-05-23 15:44:23 -0400

    Disallow viewing dashboard when not signed in

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,16 @@
-import React from "react"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import Spinner from "../components/Spinner"
 
 function Dashboard() {
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+  useEffect(() => {
+    if (!user) navigate("/login")
+  }, [user, navigate])
+
+  if (!user) return <Spinner />
   return <div>Dashboard</div>
 }
 
